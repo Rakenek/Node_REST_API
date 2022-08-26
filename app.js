0d4061cbbf1afce4bb7e9d1aa867c290920695b7
Rakenek <kamilotawski@gmail.com>
2022-08-26 20:54:04 +0200

    368

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const feedRouter = require("./routes/feed");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(feedRouter);
+//app.use(bodyParser.urlencoded({ extended: false })); //x-www-form-urlencoded
+app.use(bodyParser.json()); //application/json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRouter);
 
 app.use((req, res, next) => {
   res.status(404).send(`<h1>page not found 404</h1>`);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,18 @@
 exports.getPosts = (req, res, next) => {
-  res.send(`<p>post</p>`);
+  res.status(200).json({
+    posts: [{ title: "First Post", content: "This is the firs post!" }],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content,
+    },
+  });
 };
