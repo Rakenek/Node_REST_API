5f0c74a19f7269fbe4ec1dac44fc860cbc49bf87
Rakenek <kamilotawski@gmail.com>
2022-08-27 09:44:06 +0200

    380

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const path = require("path");
 const express = require("express");
 const feedRouter = require("./routes/feed");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { MONGO_PASS } = require("./SECRET");
+
+const MONGODB_URI = `mongodb+srv://raken:${MONGO_PASS}@cluster0.t7o7cnp.mongodb.net/messages?retryWrites=true&w=majority`;
 
 const app = express();
 
 //app.use(bodyParser.urlencoded({ extended: false })); //x-www-form-urlencoded
 app.use(bodyParser.json()); //application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -18,9 +24,19 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRouter);
+app.use((err, req, res, next) => {
+  console.log(err);
+  const status = err.statusCode || 500;
+  const message = err.message;
+  res.status(status).json({ message: message });
+});
 
 app.use((req, res, next) => {
   res.status(404).send(`<h1>page not found 404</h1>`);
 });
-
-app.listen(8080);
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => console.log(err));
