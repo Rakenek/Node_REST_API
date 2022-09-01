cea30edc889244f048a474616cabfc7ca0cb05e6
Rakenek <kamilotawski@gmail.com>
2022-09-01 23:02:23 +0200

    439

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const { MONGO_PASS } = require("./SECRET");
 const multer = require("multer");
 const { graphqlHTTP } = require("express-graphql");
+const fs = require("fs");
 
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
@@ -59,6 +60,21 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put("/post-image", (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error("Not authenticated!");
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided!" });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: "File stored", filePath: req.file.path });
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -102,3 +118,8 @@ mongoose
     // });
   })
   .catch((err) => console.log(err));
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
--- graphql/schema.js
+++ graphql/schema.js
@@ -46,7 +46,8 @@ module.exports = buildSchema(`
 
     type RootQuery {
         login(email:String!, password:String!): AuthData!
-        posts: PostData!
+        posts(page: Int): PostData!
+        post(id:ID!):Post!
     }
 
     type RootMutation {
